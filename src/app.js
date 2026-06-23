bf0ce7363adc0a8940488c85686cb417a6748a7b
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-06-23 23:59:17 +0530

    get post delate

--- src/app.js
+++ src/app.js
@@ -2,7 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user",(req,res)=>{
+    res.send({"Gourav":"kaamchor",Age:"27"});
+});
 
+app.post("/user",(req,res)=>{
+    res.send("Post method work Perfect");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Delate successfully")
+})
 
 app.use("/abc",(req,res)=>{
     res.send("abc page");
