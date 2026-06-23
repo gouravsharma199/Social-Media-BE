0e90accb104383acd453b2ec62b5df9b20286fd5
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-06-23 20:15:39 +0530

    server created

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",(req,res)=>{
+    res.send("Home page adfsd");
+});
+
+app.use("/abc",(req,res)=>{
+    res.send("abc page");
+});
+app.use("/test",(req,res)=>{
+    res.send("test page");
+});
+
+
+
+
+app.listen(222,()=>{
+    console.log("server is running on 222");
+});
