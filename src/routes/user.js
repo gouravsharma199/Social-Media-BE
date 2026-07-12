18bd3336cd165aa6569e353f444d4db040d956f8
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-07-12 19:02:35 +0530

    My connections woking fine testing is done

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionReq = require("../models/connectionReq");
 
+const SAFE_DATA = ["firstName","lastName","age","about","gender","skills"];
+
 userRouter.get("/user/request/recived",userAuth,async(req,res)=>{
     try{
         const loggedInUser = req.user;
@@ -10,7 +12,7 @@ userRouter.get("/user/request/recived",userAuth,async(req,res)=>{
         const connectionReq = await ConnectionReq.find({
         toUserId:loggedInUser,
         status:"interested"
-        }).populate("fromUserId",["firstName","lastName","age","about","gender","skills"]);
+        }).populate("fromUserId",SAFE_DATA);
 
         res.json({
         message:"data fatched successfully",
@@ -18,6 +20,31 @@ userRouter.get("/user/request/recived",userAuth,async(req,res)=>{
         })
     }catch(err){
         res.status(400).send(" error in user checking request "+err.message);
+    }
+});
+
+userRouter.get("/user/request/accepted",userAuth,async(req,res)=>{
+
+    try{
+        const loggedInUser = req.user;
+
+        const connectionReq = await ConnectionReq.find({
+            $or:[
+                {toUserId:loggedInUser._id,status:"accepted"},
+                {formUserId:loggedInUser._id,status:"accepted"}
+            ]
+        }).populate("fromUserId",SAFE_DATA).populate("toUserId",SAFE_DATA);
+
+        const data = connectionReq.map((row)=>{
+            if(row.fromUserId._id.toString()===loggedInUser._id.toString()){
+                return row.toUserId
+            }
+           return row.fromUserId
+        });
+        res.json({data});
+    }
+    catch(err){
+        res.status(400).send("Error " +err.message)
     }
 });
 
