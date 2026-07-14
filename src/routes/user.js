4639651587defefb072735f855c746566e879532
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-07-14 21:18:44 +0530

    feed api working fine

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,9 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionReq = require("../models/connectionReq");
+const connectionReq = require("../models/connectionReq");
+const { set } = require("mongoose");
+const User = require("../models/user");
 
 const SAFE_DATA = ["firstName","lastName","age","about","gender","skills"];
 
@@ -31,7 +34,7 @@ userRouter.get("/user/request/accepted",userAuth,async(req,res)=>{
         const connectionReq = await ConnectionReq.find({
             $or:[
                 {toUserId:loggedInUser._id,status:"accepted"},
-                {formUserId:loggedInUser._id,status:"accepted"}
+                {fromUserId:loggedInUser._id,status:"accepted"}
             ]
         }).populate("fromUserId",SAFE_DATA).populate("toUserId",SAFE_DATA);
 
@@ -47,5 +50,40 @@ userRouter.get("/user/request/accepted",userAuth,async(req,res)=>{
         res.status(400).send("Error " +err.message)
     }
 });
+
+userRouter.get("/user/feed",userAuth,async(req,res)=>{
+    try{
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page)|| 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page-1)*limit;
+            
+        const connectionReq = await ConnectionReq.find({
+        $or:[{fromUserId:loggedInUser._id},
+            {toUserId:loggedInUser._id}]
+        }).select("fromUserId toUserId");
+        //select is filter the other data only showing the id
+        console.log(connectionReq);
+        //Set() is used to remove duplicate data to store
+        const hideUserFeed = new Set();
+
+        connectionReq.forEach((req)=> {
+            hideUserFeed.add(req.fromUserId.toString());
+            hideUserFeed.add(req.toUserId.toString());
+        });
+         
+        const users = await User.find({
+            $and:[
+                {_id:{$nin: Array.from(hideUserFeed)}},
+                {_id:{$ne:loggedInUser._id}}
+            ]
+        }).select(SAFE_DATA).skip().limit();
+        console.log(users);
+
+        res.send(users);
+    }catch(err){
+        res.status(400).json({message:"ERR"+err.message});
+    }
+})
 
 module.exports = userRouter;
